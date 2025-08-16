4e515f02575c80b4c9cb9ad2cfea843f3505453a
Nazmus Sadat <151758467+Nazmus-Sadat-21@users.noreply.github.com>
2025-08-17 01:38:10 +0600

    Add files via upload

---
+++ PRACTICE/Lab_Task_1/Lab_Task_1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello");
+            //Console.WriteLine("Nazmus Sadat");
+
+            //double a = 5.5;
+            //double b = 6.7;
+            //double result = a + b;
+            //Console.WriteLine($"The sum of two number is {result}");
+
+            //double a = 5.5;
+            //double b = 6.7;
+            //double result = a / b;
+            //Console.WriteLine($"The division of two number is {result}");
+
+            //double a = -5 + 8 * 6;
+            //double b = (55 + 9) % 9;
+            //double c = 20 + -3 * 5 / 8;
+            //double d = 5 + 15 / 3 * 2 - 8 % 3;
+            //Console.WriteLine($"The results are : \n{a}\n{b}\n{c}\n{d}");
+
+            //double a = 7.78;
+            //double b = 5.984;
+            //Console.WriteLine($"Addition of two numbers {a+b}");
+            //Console.WriteLine($"Substraction of two numbers {a - b}");
+            //Console.WriteLine($"Multiplication of two numbers {a * b}");
+            //Console.WriteLine($"Division of two numbers {a / b}");
+            //Console.WriteLine($"Module of two numbers {a % b}");
+
+            //double result = (25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5);
+            //Console.WriteLine("Result: " + result);
+
+            //double Width = 5.5 ,Height = 8.5;
+            //Console.WriteLine($"Area : {Width * Height}");
+            //Console.WriteLine($"Perimeter : {2*(Width + Height)}");
+
+            //int first = 25;
+            //int second = 39;
+
+            //if (first == second)
+            //    Console.WriteLine(first + " == " + second);
+            //if (first != second)
+            //    Console.WriteLine(first + " != " + second);
+            //if (first < second)
+            //    Console.WriteLine(first + " < " + second);
+            //if (first <= second)
+            //    Console.WriteLine(first + " <= " + second);
+            //if (first > second)
+            //    Console.WriteLine(first + " > " + second);
+            //if (first >= second)
+            //    Console.WriteLine(first + " >= " + second);
+
+            //Console.Write("Enter a string: ");
+            //string input = Console.ReadLine();        
+            //string reversed = "";
+            //for (int i = input.Length - 1; i >= 0; i--)
+            //{
+            //    reversed += input[i];
+            //}
+            //Console.WriteLine($"Reversed string: {reversed}");
+
+            //Console.WriteLine("Enter two numbers : ");
+            //double a = double.Parse(Console.ReadLine());
+            //double b = double.Parse(Console.ReadLine());
+            //if ((a > 0 && a < 1) && (b > 0 && b < 1))
+            //{
+
+            //    Console.WriteLine("True");
+            //}
+            //else
+            //{
+
+            //    Console.WriteLine("False");
+
+            //}
+
+        }
+    }
+}
