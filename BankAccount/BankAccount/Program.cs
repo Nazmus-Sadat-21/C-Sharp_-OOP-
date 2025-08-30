7c4298debe6a5da5a058b776ee61405c626b40f0
Nazmus Sadat <151758467+Nazmus-Sadat-21@users.noreply.github.com>
2025-08-31 05:56:32 +0600

    Update Program.cs

--- BankAccount/BankAccount/Program.cs
+++ BankAccount/BankAccount/Program.cs
@@ -12,6 +12,10 @@ namespace BankAccount
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+
+
+            
         }
     }
+
 }
